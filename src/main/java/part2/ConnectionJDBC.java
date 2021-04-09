f14debf58009c8ddc8417268438199f5296c407f
luizsac <lu_que90@hotmail.com>
2021-04-09 15:27:07 -0300

    Base project

---
+++ src/main/java/part2/ConnectionJDBC.java
@@ -0,0 +1,67 @@
+package part2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+
+    public static void main(String[] args) {
+
+        String driver = "postgresql";
+        String dbAddress = "localhost";
+        String dbName = "digital_innovation_one";
+        String user = "postgres";
+        String password = "ringolingo";
+
+        StringBuilder sb = new StringBuilder("jdbc:")
+                .append(driver).append("//")
+                .append(dbAddress).append("/")
+                .append(dbName);
+
+        String urlConnection = sb.toString();
+
+        try (Connection conn = DriverManager.getConnection(urlConnection, user, password)) {
+            System.out.println("Sucesso!");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            System.out.println("Erro!");
+        }
+
+//        // 1 - NÃO ESQUECER DE BAIXAR O DRIVER PARA O BANCO DE DADOS QUE IRÁ UTILIZAR! (Como demonstrado na parte 1 do curso)
+//
+//        // 2 - Definir parâmetros para se conectar ao banco de dados MySQL.
+////        String driver = "mysql";
+////        String dataBaseAddress = "localhost";
+////        String dataBaseName = "digital_innovation_one";
+////        String user = "root";
+////        String password = "password";
+//
+//        // Conexão usando PostgreSQL
+//        String driver = "postgresql";
+//        String dataBaseAddress = "localhost";
+//        String dataBaseName = "digital_innovation_one";
+//        String user = "postgres";
+//        String password = "ringolingo";
+//
+//        // 3 - Construção da string de conexão.
+//        StringBuilder sb = new StringBuilder("jdbc:")
+//                .append(driver).append("://")
+//                .append(dataBaseAddress).append("/")
+//                .append(dataBaseName);
+//
+//        String connectionUrl = sb.toString(); //sb.toString() == "jdbc:mysql://localhost/digital_innovation_one"
+//
+//        //Carregar a classe específica de implementação do driver na memória da JVM. (A partir da versão JDBC 4 não é mais necessário!!!)
+//        //Class.forName("com.mysql.jdbc.Driver");
+//
+//        // 4 - Criar conexão usando o DriverManager, passando como parâmetros a string de conexão, usuário e senha do usuário.
+//        try (Connection conn = DriverManager.getConnection(connectionUrl, user, password)) {
+//            System.out.println("SUCESSO ao se conectar ao banco PostgreSQL!");
+//        } catch (SQLException e) {
+//            System.out.println("FALHA ao se conectar ao banco PostgreSQL!");
+//            e.printStackTrace();
+//        }
+
+    }
+}
